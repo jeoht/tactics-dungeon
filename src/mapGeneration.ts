1dcb9b59b67aef95fb092b2c95d079353b88ab23
Jake Leoht <jake.leoht@gmail.com>
2020-03-15 01:59:42 +1100

    Initial map generation

--- src/mapGeneration.ts
+++ src/mapGeneration.ts
@@ -1,3 +1,8 @@
+import { makeGrid, randomPos } from "./util";
+import { Block, MapBase } from "./MapBase";
+import { BOARD_ROWS, BOARD_COLS } from "./settings";
+import { Biome } from "./Tile";
+import _ = require("lodash");
 
 
 interface CellDef {
@@ -10,6 +15,39 @@ interface MapgenSettings {
 
 }
 
-export function generateMap(props: MapgenSettings = {}) {
+function randomBlocks(): Block[] {
+    if (Math.random() > 0.8) { 
+        return [Block.Floor, Block.Wall]
+    } else {
+        return [Block.Floor]
+    }
+}
 
+export function generateMap(props: MapgenSettings = {}) {
+    const biome = _.sample(Object.values(Biome)) as Biome
+
+    const [width, height] = [BOARD_COLS, BOARD_ROWS]
+    const blocks = makeGrid<Block[]>(width, height)
+
+
+    for (let i = 0; i < width; i++) {
+        for (let j = 0; j < height; j++) {
+            blocks[i][j] = randomBlocks()
+        }
+    }
+
+    const enemySpawns = 8
+    const teamSpawns = 6
+
+    for (const i of _.range(enemySpawns)) {
+        const { x, y } = randomPos(width, height)
+        blocks[x][y].push(Block.EnemySpawn)
+    }
+
+    for (const i of _.range(teamSpawns)) {
+        const { x, y } = randomPos(width, height)
+        blocks[x][y].push(Block.PlayerSpawn)
+    }
+
+    return new MapBase({ biome, blocks })
 }
--- src/util.ts
+++ src/util.ts
@@ -1,5 +1,18 @@
 import { v4 as uuidv4 } from 'uuid'
+import { PointVector } from './PointVector'
 
 export function uuid() {
     return uuidv4()
+}
+
+export function makeGrid<T>(width: number, height: number, mapfn?: () => T) {
+    const grid: T[][] = []
+    for (let i = 0; i < width; i++) {
+        grid[i] = Array.from({ length: height }, mapfn ? mapfn as any : () => undefined) 
+    }
+    return grid
+}
+
+export function randomPos(width: number, height: number) {
+    return new PointVector(Math.floor(Math.random()*width), Math.floor(Math.random()*height))
 }
