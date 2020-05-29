5b71629b97a00cb1bddf08bdf8eb6cbd4e0adef9
Mispy <mispy@mispy.me>
2020-05-29 11:13:04 +1000

    AudioBuffer variant of sound loading

--- src/soundboard.ts
+++ src/soundboard.ts
@@ -1,27 +1,42 @@
+import _ = require("lodash")
+
 const soundUrls = {
     footstep: '/audio/footstep01.ogg'
 }
 
-export type Soundboard = Record<keyof typeof soundUrls, HTMLAudioElement>
+export class Sound {
+    constructor(readonly context: AudioContext, readonly buffer: AudioBuffer) {
+    }
+
+    play() {
+        const source = this.context.createBufferSource()
+        source.buffer = this.buffer
+        source.connect(this.context.destination)
+        source.start()
+    }
+}
+
+export type Soundboard = Record<keyof typeof soundUrls, Sound>
 
 export async function loadSounds(): Promise<Soundboard> {
-    return new Promise((resolve, reject) => {
-        const sounds: { [key: string]: HTMLAudioElement } = {}
-        for (const [key, value] of Object.entries(soundUrls)) {
-            const audio = new Audio()
-            audio.addEventListener('canplaythrough', onload)
-            audio.src = value
-            audio.volume = 0.5
-            sounds[key] = audio
-        }
+    const context = new AudioContext()
 
-        const toLoad = Object.keys(soundUrls).length
-        let loadedSounds = 0
-        function onload() {
-            loadedSounds += 1
-            if (loadedSounds === toLoad) {
-                resolve(sounds as Soundboard)
+    return new Promise((resolve, reject) => {
+        const sounds: { [key: string]: Sound } = {}
+        for (const [key, url] of Object.entries(soundUrls)) {
+            const request = new XMLHttpRequest()
+            request.open('GET', url, true)
+            request.responseType = 'arraybuffer'
+
+            request.onload = () => {
+                context.decodeAudioData(request.response, buffer => {
+                    sounds[key] = new Sound(context, buffer)
+                    if (_.size(sounds) === _.size(soundUrls)) {
+                        resolve(sounds as Soundboard)
+                    }
+                })
             }
+            request.send();
         }
     })
 }
